18fc034552b36c1f030592e59553b3ef773522b4
Kaushik N <kaushik1603n@gmail.com>
2025-07-06 10:08:57 +0530

    change some file like docker and app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -71,7 +71,7 @@ export class App {
             "http://localhost",
             "http://127.0.0.1",
             "http://client:80",
-            "http://34.47.233.51",
+            "http://34.47.233.51:3000",
           ];
           if (!origin || allowedOrigins.includes(origin)) {
             callback(null, origin || "*");
