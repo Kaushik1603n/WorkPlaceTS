90db56c8145e0d28daf69f84b5076be4cd704b9c
Kaushik N <kaushik1603n@gmail.com>
2025-08-16 14:50:08 +0530

    userRepo updated

--- server/src/domain/interfaces/IuserRepo.ts
+++ server/src/domain/interfaces/IuserRepo.ts
@@ -12,33 +12,3 @@ export interface userRepoI {
   updateEmail(userId: string | unknown, email: string): Promise<any>;
   updateName(userId: string | unknown, fullName: string): Promise<any>;
 }
-
-// export interface IUser {
-//   fullName: string;
-//   email: string;
-//   password?: string;
-//   role: UserRole;
-//   isVerified: boolean;
-//   otp?: number;
-//   otpExpiry?: Date;
-//   refreshToken?: string|null;
-//   googleId?: string;
-//   pic?: string;
-//   status: UserStatus;
-//   lastLogin?: Date;
-//   jobs?: any[];
-//   profile?: any;
-//   createdAt?: Date;
-//   updatedAt?: Date;
-// }
-// export enum UserRole {
-//   FREELANCER = "freelancer",
-//   CLIENT = "client",
-//   ADMIN = "admin",
-// }
-
-// export enum UserStatus {
-//   ACTIVE = "active",
-//   SUSPENDED = "suspended",
-//   DELETED = "deleted",
-// }
